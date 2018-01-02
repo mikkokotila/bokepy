9209f439656e5da01680ee290bbec707b05872e2
Mikko Kotila <mailme@mikkokotila.com>
2018-01-02 20:04:45 +0200

    Add files via upload

---
+++ boke/__init__.py
@@ -0,0 +1,7 @@
+"""
+Main module for the grapheme package.
+"""
+
+from .api import graphemes, length, grapheme_lengths, slice, contains, safe_split_index, startswith, endswith
+
+__all__ = ['graphemes', 'length', 'grapheme_lengths', 'slice', 'contains', 'safe_split_index', 'startswith', 'endswith']
