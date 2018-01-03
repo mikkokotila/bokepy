707da88bdae6ad94b4728ce5d649fe20d1f8ed3f
Mikko Kotila <mailme@mikkokotila.com>
2018-01-04 00:09:48 +0200

    cleaned up the repo

--- boke/__init__.py
+++ boke/__init__.py
@@ -1,7 +1,1 @@
-"""
-Main module for the grapheme package.
-"""
-
-from .api import graphemes, length, grapheme_lengths, slice, contains, safe_split_index, startswith, endswith
-
-__all__ = ['graphemes', 'length', 'grapheme_lengths', 'slice', 'contains', 'safe_split_index', 'startswith', 'endswith']
+__version__ = "0.1"
